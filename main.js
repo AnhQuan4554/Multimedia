3830afabbe4cc9a737ed0e4920082d241c57c26a
Quân <quanmanchester0405@gmail.com>
2022-11-03 10:41:21 +0700

    3/11

--- main.js
+++ main.js
@@ -20,19 +20,75 @@ imgChoice.addEventListener("click", () => {
 const infor_teacher_item = document.querySelectorAll(".infor_teacher_item");
 const btnCloseInforDetail = document.querySelectorAll(".btnCloseInforDetail");
 const inforDetail = document.querySelectorAll(".inforDetail");
+const placeText = document.querySelector(".placeText");
+
+const searchInforInput = document.querySelector(".searchInfor input");
+const imgNewWriter_item_left = document.querySelector(
+  ".newWriter_item-left img"
+);
+const imgNewWriter_item_right = document.querySelectorAll(
+  ".newWriter_item-right img"
+);
+const arrangeWriter_items = document.querySelectorAll(
+  ".arrangeWriter_item img"
+);
+//////////////////////////////
 infor_teacher_item.forEach((item, index) => {
   item.addEventListener("click", () => {
     inforDetail[index].style.display = "flex";
   });
 });
-// inforDetail.forEach((item) => {});
-// inforDetail.forEach((item,index) => {
-//   item.addEventListener("click", () => {
-//     item.style.display = "none";
-//   });
-// });
 btnCloseInforDetail.forEach((item, index) => {
   item.addEventListener("click", () => {
     inforDetail[index].style.display = "none";
   });
 });
+const iconChoice = document.querySelector(".iconChoice");
+
+document.addEventListener("scroll", (event) => {
+  // console.log(iconChoice.getBoundingClientRect().top, window.scrollY);
+  if (iconChoice.getBoundingClientRect().top - window.scrollY < 350) {
+    iconChoice.classList.add("active");
+  } else {
+    iconChoice.classList.remove("active");
+  }
+});
+const infor_teacher_items = document.querySelectorAll(".infor_teacher_item");
+document.addEventListener("scroll", (event) => {
+  // console.log(infor_teacher_items[0]);
+  infor_teacher_items.forEach((item) => {
+    if (item.getBoundingClientRect().top - window.scrollY < 350) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+  if (searchInforInput.getBoundingClientRect().top - window.scrollY < 350) {
+    searchInforInput.classList.add("active");
+    // Xuất hiện text
+
+    setTimeout(() => {
+      placeText.style.display = "block";
+    }, 2000);
+  } else {
+    searchInforInput.classList.remove("active");
+    placeText.style.display = "none";
+  }
+  // img other Page
+  if (
+    imgNewWriter_item_right[0].getBoundingClientRect().top - window.scrollY <
+    -500
+  ) {
+    imgNewWriter_item_right[0].classList.add("active");
+    imgNewWriter_item_right[1].classList.add("active");
+    imgNewWriter_item_left.classList.add("active");
+  } else {
+    imgNewWriter_item_right[0].classList.remove("active");
+    imgNewWriter_item_right[1].classList.remove("active");
+    imgNewWriter_item_left.classList.remove("active");
+  }
+});
+// document.addEventListener("scroll", (event) => {
+//   if(arrangeWriter_items[0])
+
+// });
